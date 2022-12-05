4686361fc9311235e59122c6d5bd968513804601
RIFQIICHSAN <rifqiican872@gmail.com>
2022-12-05 20:21:23 +0700

    simplerestfull

---
+++ src/main/java/pws/b/demo/CobaPostApiApplication.java
@@ -0,0 +1,13 @@
+package pws.b.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaPostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaPostApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/b/demo/CobaPostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.b.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaPostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
